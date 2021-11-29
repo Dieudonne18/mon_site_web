23f3a39f948e737b5235ed123045b04f40323062
Dieudonne18 <91911211+Dieudonne18@users.noreply.github.com>
2021-11-29 16:46:19 +0100

    Add files via upload

--- index_html.php
+++ index_html.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="./css/style.css">
     <link rel="icon" href="./images/5.jpg" width:32px,height:32px>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="./formul.js" async></script>
+    <script src="./form.js" async></script>
 
 </head>
 
@@ -50,7 +50,7 @@
                     <legend>IMAGE:</legend>
                     <div class="mb-3">
                         <div class="image_recette">
-                            <h2>Formulaire ajout une image</h2>
+                            <h2> <em>Ajout image</em></h2>
                             <label for="Photo">Ajouter une photo: </label>
                             <br><br>
                             <input type="file" name="photo" id="fichier" multiple value="photo">
@@ -61,33 +61,29 @@
                 </article>
                 <hr>
                 <article>
-                    <!-- ********************ETAPE INFO********************* -->
+                    <!-- ********************ETAPE INFO ********************* -->
                     <legend>INFOS CLÉS</legend>
                     <div class="mb-3">
-                        <div class="dificul" style="text-align:left">Difficulté</div>
-                        <!-- <select class="form-select" name="difficulte"> -->
-                        <!-- <option selected>Difficulté</option> -->
+                        <div class="dificul" style="text-align:left"><em>Difficulté</em></div>
                         <input name="difficulte" type="radio">Tres facile<br>
                         <input name="difficulte" type="radio">Facile<br>
                         <input name="difficulte" type="radio">moyenne<br>
                         <input name="difficulte" type="radio">difficile<br>
                         <input name="difficulte" type="radio">tres difficile<br>
-                        <!-- </select> -->
                     </div>
                 </article>
                 <hr>
+                <!-- *******************cout************* -->
                 <div class="mb-3">
-                    <div class="cou" style="text-align:left">Cout</div>
-                    <!-- <select class="form-select" name="cout"> -->
-                    <!-- <option selected>coût</option> -->
+                    <div class="cou" style="text-align:left"><em>Cout</em></div>
                     <input name="cout" type="radio">Bon marché<br>
                     <input name="cout" type="radio">Budget moyen<br>
                     <input name="cout" type="radio">Assez cher<br>
                 </div>
-                <!-- </div> -->
                 <hr>
+                <!-- **********************pays d'origine*********** -->
                 <div class="mb-3">
-                    <label for="pays"><em>pays d'origine : </em></label>
+                    <label for="pays"><em>Pays d'origine : </em></label>
                     <select id="Pays" name="pays">
                         <!-- <label for="title">Pays d'origine</label> -->
                         <!-- <option selected>pays</option> -->
@@ -107,7 +103,7 @@
                     <div class="mb-3">
                         <option selected>temps de preparation.</option>
                         <br>
-                        <label for="temps">Heure : </label>
+                        <label for="temps"><em>Heure et Minuite :</em> </label>
                         <input type="time" id="temps" name="temps" min="0" max="24">
                     </div>
                 </article>
@@ -116,7 +112,7 @@
                     <div class="mb-3">
                         <option selected>Temps de cuisson</option>
                         <br>
-                        <label for="temps">Heure : </label>
+                        <label for="temps"><em>Heure et Minuite :</em></label>
                         <input type="time" id="temps" name="temps2" min="0" max="24">
                     </div>
                 </article>
@@ -126,8 +122,7 @@
                     <div class="mb-3">
                         <option selected>temps d'attente</option>
                         <br>
-
-                        <label for="temps">Heure : </label>
+                        <label for="temps"><em>Heure et Minuite :</em> </label>
                         <input type="time" id="temps" name="temps4" min="0" max="24">
                     </div>
                 </article>
@@ -135,14 +130,11 @@
                 <article>
                     <!-- **************ETAPE INGREDIENTS************** -->
                     <legend>PERSONNE</legend>
-                    <!-- <div class="mb-3"> -->
-                    <h2>Nombre de personnes ou portions *</h2>
+                    <h2><em>Nombre de personnes ou portions </em></h2>
                     <div class="perso">
                         <div class="lapin">
                             <label for="number"></label>
                             <input type="number" id="num" name="NBPersonne" min="0" max="24">
-                            <!-- </div> -->
-                            <!-- <div class="selec"> -->
                             <select class="personne" name="NBPersonne">
                                 <option>---</option>
                                 <!-- <option selected>Personne</option> -->
@@ -150,7 +142,6 @@
                                 <option value="Portion">Portion</option>
                                 <option value="Biscuit">Biscuit</option>
                             </select>
-                            <!-- <input type="number" id="text" min="0" max="1000"> -->
                         </div>
                     </div>
                 </article>
@@ -160,10 +151,9 @@
                         <div class=etapes>
                             <!-- ****************ETAPE CUISSON***************** -->
                             <legend>Étapes Prèparation</legend>
-                            <h2>Si votre recette nécessite une cuisson au four,
-                                veuillez indiquer la température de cuisson.</h2>
+                            <h2><em>Si votre recette nécessite une cuisson au four,
+                                    veuillez indiquer la température de cuisson.</em></h2>
                         </div>
-                        <!-- <legend>Etapes</legend> -->
                         <br>
                         <div class="etap" name="etape">
                             <textarea name="etape" type="text" id="texte" cols="50" rows="5"></textarea>
@@ -174,27 +164,52 @@
                                 <figcaption>Image repas du jour</figcaption>
                             </figure>
                         </div>
+                        <!-- ***********etape creation des etapes**************** -->
                         <div class="bout">
-                            <br> Voici les etape n°1 :
+                            <br><em> Les etapes de cuisson :</em>
                             <input id="Bouton" name='etape' placeholder="Press me !">
-                            <div id="tonDiv">Etape1</div>
-                            <div id="tonDiv2">Etape2</div>
+                            <!-- <div id="tonDiv">Etape1</div>
+                            <div id="tonDiv2">Etape2</div> -->
                             <script>
-                                document.querySelector("#Bouton").onclick = function() {
-                                    if (window.getComputedStyle(document.querySelector('#tonDiv')).display == 'none') {
-                                        document.querySelector("#tonDiv").style.display = "block";
-                                    } else {
-                                        document.querySelector("#tonDiv").style.display = "none";
-                                    }
-                                    if (window.getComputedStyle(document.querySelector('#tonDiv')).display == 'none') {
-                                        document.querySelector("#tonDiv2").style.display = "block";
-                                    } else {
-                                        document.querySelector("#tonDiv2").style.display = "none";
+                                let numeroEtape = 1;
+                                let btn = document.getElementById("Bouton");
+                                btn.addEventListener('click', Boite);
 
-                                    }
+                                function Boite() {
+
+                                    let div1 = document.createElement('input');
+                                    div1.type = "textarea";
+                                    div1.style.width = '700px';
+                                    div1.style.height = '100px';
+                                    div1.style.background = '#c0a1a1';
+                                    div1.style.border = "2px solid black";
+                                    let bout = document.querySelector(".bout");
+
+
+                                    // for (i = 1; i <= 8; i++) {
+                                    div1.placeholder = "etape" + numeroEtape;
+                                    numeroEtape++;
+
+                                    // div1.placeholder = "etape" + i;
+                                    // }
+                                    bout.appendChild(div1);
+
                                 }
+
+                                // document.querySelector("#Bouton").onclick = function() {
+                                //     if (window.getComputedStyle(document.querySelector('#tonDiv')).display == 'none') {
+                                //         document.querySelector("#tonDiv").style.display = "block";
+                                //     } else {
+                                //         document.querySelector("#tonDiv").style.display = "none";
+                                //     }
+                                //     if (window.getComputedStyle(document.querySelector('#tonDiv')).display == 'none') {
+                                //         document.querySelector("#tonDiv2").style.display = "block";
+                                //     } else {
+                                //         document.querySelector("#tonDiv2").style.display = "none";
+
+                                //     }
+                                // }
                             </script>
-                            <!-- <input onclick="go" placeholder="Go !"></input> -->
                         </div>
                     </div>
                     <div class="mb-3" id="d1">
@@ -231,8 +246,8 @@
                     </p> -->
                     </div>
                 </article>
-                <!-- **********************ETAPE CONSEILS ET MESSAGE**************** -->
                 <hr>
+                <!-- **********************ETAPE CONSEILS ET MESSAGE**************** -->
                 <article>
                     <legend>CONSEIL</legend>
                     <div class="mb-3">
